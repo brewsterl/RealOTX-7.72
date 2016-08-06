de6c25f43bb1d3f69cca86146d4a1196bf5c3dd6
Azuen <brunohenrique_999@hotmail.com>
2016-08-06 17:48:41 -0300

    doPlayerPopupFYI doesn't exist
    Fixing this one with another function that exists.

--- data/talkactions/scripts/serverinfo.lua
+++ data/talkactions/scripts/serverinfo.lua
@@ -19,6 +19,7 @@ function onSay(cid, words, param, channel)
 		exp = getExperienceStage(getPlayerLevel(cid), getVocationInfo(getPlayerVocation(cid)).experienceMultiplier)
 	end
 
-	doPlayerPopupFYI(cid, "Server Information:\n\nExperience rate: x" .. exp .. "\nSkills rate: x" .. config.rateSkill .. "\nLoot rate: x" .. config.rateLoot .. "\nMagic rate: x" .. config.rateMagic .. "\nSpawns rate: x" .. config.rateSpawnMin .. " - x" .. config.rateSpawnMax .. "\nProtection level: " .. config.protectionLevel)
+	local info = "Server Information:\n\nExperience rate: x" .. exp .. "\nSkills rate: x" .. config.rateSkill .. "\nLoot rate: x" .. config.rateLoot .. "\nMagic rate: x" .. config.rateMagic .. "\nSpawns rate: x" .. config.rateSpawnMin .. " - x" .. config.rateSpawnMax .. "\nProtection level: " .. config.protectionLevel
+	doShowTextDialog(cid, ITEM_ACTION_BOOK, info)
 	return true
 end
