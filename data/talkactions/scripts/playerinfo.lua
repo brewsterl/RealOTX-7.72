508bbba96b2998f752a993ecb76a5fae322cb7f4
Azuen <brunohenrique_999@hotmail.com>
2016-08-06 18:43:59 -0300

    Fixing talkaction /playerinfo doPlayerPopupFYI
    with doShowTextDialog

--- data/talkactions/scripts/playerinfo.lua
+++ data/talkactions/scripts/playerinfo.lua
@@ -12,27 +12,7 @@ function onSay(cid, words, param, channel)
 
 	local tmp = {accountId = getPlayerAccountId(pid), ip = getPlayerIp(pid)}
 	local pos = getCreaturePosition(pid)
-	doPlayerPopupFYI(cid, "Information about player" ..
-		"\nName: " .. getCreatureName(pid) ..
-		"\nGUID: " .. getPlayerGUID(pid) ..
-		"\nGroup: " .. getPlayerGroupName(pid) ..
-		"\nAccess: " .. getPlayerAccess(pid) ..
-		"\nVocation: " .. getVocationInfo(getPlayerVocation(pid)).name ..
-		"\nStatus:" ..
-			"\nLevel - " .. getPlayerLevel(pid) .. ", Magic Level - " .. getPlayerMagLevel(pid) .. ", Speed - " .. getCreatureSpeed(pid) ..
-			"\nHealth - " .. getCreatureHealth(pid) .. " / " .. getCreatureMaxHealth(pid) .. ", Mana - " .. getCreatureMana(pid) .. " / " .. getCreatureMaxMana(pid) ..
-			"\nSkills:" ..
-			"\nFist - " .. getPlayerSkillLevel(pid, SKILL_FIST) .. ", Club - " .. getPlayerSkillLevel(pid, SKILL_CLUB) .. ", Sword - " .. getPlayerSkillLevel(pid, SKILL_SWORD) .. ", Axe - " .. getPlayerSkillLevel(pid, SKILL_AXE) ..
-			"\nDistance - " .. getPlayerSkillLevel(pid, SKILL_DISTANCE) .. ", Shielding - " .. getPlayerSkillLevel(pid, SKILL_SHIELD) .. ", Fishing - " .. getPlayerSkillLevel(pid, SKILL_FISHING) ..
-		"\nCoins:" ..
-			"\nCrystal - " .. getPlayerItemCount(pid, ITEM_CRYSTAL_COIN) .. ", Platinum - " .. getPlayerItemCount(pid, ITEM_PLATINUM_COIN) .. ", Gold - " .. getPlayerItemCount(pid, ITEM_GOLD_COIN) ..
-			"\nOverall amount - " .. getPlayerMoney(pid) ..
-		"\nBalance: " .. getPlayerBalance(pid) ..
-		"\nPosition: [X - " .. pos.x .. " | Y - " .. pos.y .. " | Z - " .. pos.z .. "]" ..
-		"\n\nInformation about account" ..
-		"\nName: " .. getPlayerAccount(pid) ..
-		"\nID: " .. tmp.accountId ..
-		"\nNotations: " .. getNotationsCount(tmp.accountId) ..
-		"\nIP: " .. doConvertIntegerToIp(tmp.ip) .. " (" .. tmp.ip .. ")")
+	local info = "Information about player" .. "\nName: " .. getCreatureName(pid) .. "\nGUID: " .. getPlayerGUID(pid) .. "\nGroup: " .. getPlayerGroupName(pid) .. "\nAccess: " .. getPlayerAccess(pid) .. "\nVocation: " .. getVocationInfo(getPlayerVocation(pid)).name .. "\nStatus:" ..	"\nLevel - " .. getPlayerLevel(pid) .. ", Magic Level - " .. getPlayerMagLevel(pid) .. ", Speed - " .. getCreatureSpeed(pid) .."\nHealth - " .. getCreatureHealth(pid) .. " / " .. getCreatureMaxHealth(pid) .. ", Mana - " .. getCreatureMana(pid) .. " / " .. getCreatureMaxMana(pid) .. "\nSkills:" .. "\nFist - " .. getPlayerSkillLevel(pid, SKILL_FIST) .. ", Club - " .. getPlayerSkillLevel(pid, SKILL_CLUB) .. ", Sword - " .. getPlayerSkillLevel(pid, SKILL_SWORD) .. ", Axe - " .. getPlayerSkillLevel(pid, SKILL_AXE) .. "\nDistance - " .. getPlayerSkillLevel(pid, SKILL_DISTANCE) .. ", Shielding - " .. getPlayerSkillLevel(pid, SKILL_SHIELD) .. ", Fishing - " .. getPlayerSkillLevel(pid, SKILL_FISHING) .. "\nCoins:" .. "\nCrystal - " .. getPlayerItemCount(pid, ITEM_CRYSTAL_COIN) .. ", Platinum - " .. getPlayerItemCount(pid, ITEM_PLATINUM_COIN) .. ", Gold - " .. getPlayerItemCount(pid, ITEM_GOLD_COIN) .. "\nOverall amount - " .. getPlayerMoney(pid) .. "\nBalance: " .. getPlayerBalance(pid) .. "\nPosition: [X - " .. pos.x .. " | Y - " .. pos.y .. " | Z - " .. pos.z .. "]" .. "\n\nInformation about account" .. "\nName: " .. getPlayerAccount(pid) .. "\nID: " .. tmp.accountId .. "\nNotations: " .. getNotationsCount(tmp.accountId) .. "\nIP: " .. doConvertIntegerToIp(tmp.ip) .. " (" .. tmp.ip .. ")"
+	doShowTextDialog(cid, ITEM_ACTION_BOOK, info)
 	return true
 end
